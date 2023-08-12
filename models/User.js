ff6ee84f93a466c04185c095b9ad46761ba9b019
BerkiManman <berkayguler7@hotmail.com>
2023-08-12 15:03:52 +0300

    naming fix

--- models/User.js
+++ models/User.js
@@ -51,7 +51,7 @@ const UserSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: 'Form',
         },
-        answers: [
+        questionAnswers: [
             {
                 question: {
                     type: Schema.Types.ObjectId,
